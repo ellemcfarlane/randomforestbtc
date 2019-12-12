6900002ec3f3155e11ec7b1ed1d274bdcf8176a8
Elle <ellesummer212@gmail.com>
2019-12-11 21:34:46 -0500

    many small changes

--- other_models/prebuilt_random_forest.py
+++ other_models/prebuilt_random_forest.py
@@ -5,48 +5,91 @@ from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.ensemble import RandomForestRegressor
 from sklearn import metrics
+from matplotlib import pyplot as plt
+from pprint import pprint
+from sklearn.model_selection import RandomizedSearchCV
 
-#dataset = pd.read_csv('petrol_consumption.csv')
-# print(dataset.head())
+#############################################################################
 
-class RandomTreeBTC:
-    def __init__(self, x):
-        self.x = 10
-
-def load_data_frame(path):
-    with open(path, 'rb') as file:
-        return pickle.load(file)
+# Old BTC
 
 # divide data into points and labels
-dataset = load_data_frame('data_frames/petrol_df.pkl')
-X = dataset.iloc[:, 0:4].values
-y = dataset.iloc[:, 4].values
+# dataset = pd.read_csv('../raw_csvs/df_final.csv', low_memory=True)
+# dataset['Price'] = dataset['Price'].shift(3)
+# dataset.drop(0, axis=0, inplace=True)
+# dataset.drop(1, axis=0, inplace=True)
+# dataset.drop(2, axis=0, inplace=True)
+# y_orig = dataset.loc[:, 'Price'].values[::-1]
+# X_orig = dataset.drop(columns=['Unnamed: 0', 'Date', 'Price']).reindex(index=dataset.index[::-1]).to_numpy()
+# dataset = dataset.sample(frac=1, random_state=0)
+# y = dataset.loc[:, 'Price'].values
+# print(dataset.head())
+# dataset.drop(columns=['Unnamed: 0', 'Date', 'Price'], inplace=True)
+
+#############################################################################
+
+# New BTC
+
+dataset = pd.read_csv('../raw_csvs/bitcoin_final.csv', low_memory=True)
+dataset['market_price'] = dataset['market_price'].shift(-1)
+dataset.drop(len(dataset) - 1, axis=0, inplace=True)
+y_orig = dataset.loc[:, 'market_price'].values
+#X_orig = dataset.drop(columns=['market_price', 'date', 'Unnamed: 0']).to_numpy()
+X_orig = dataset.drop(columns=['Unnamed: 0', 'date', 'market_price', 'n_transaction_exclude_popular']).to_numpy()
+print(dataset.head())
+dataset = dataset.sample(frac=1, random_state=0)
+y = dataset.loc[:, 'market_price'].values
+#dataset.drop(columns=['market_price', 'date', 'Unnamed: 0'], inplace=True)
+dataset.drop(columns=['Unnamed: 0', 'date', 'market_price', 'n_transaction_exclude_popular'], inplace=True)
+print(dataset.head())
+
+
+X = dataset.to_numpy()
 # split into training vs test attributes/labels
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=.2, shuffle=False, random_state=0)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=.2, shuffle=False)
 
-with open("x_train_pre", 'w') as file:
-    for point in X_train:
-        print(point, file=file)
-with open("y_train_pre", 'w') as file:
-    y = str(y_train).split()
-    for val in y:
-        print(val, file=file)
-# scale values
-# sc = StandardScaler()
-# X_train = sc.fit_transform(X_train)
-# X_test = sc.transform(X_test)
-
-# train
 regressor = RandomForestRegressor(n_estimators=200, random_state=0)
 regressor.fit(X_train, y_train)
+#pprint(regressor.get_params())
 y_pred = regressor.predict(X_test)
+y_train_pred = regressor.predict(X_train)
+y_full = regressor.predict(X_orig)
 
 
-with open("y_pred_pre", "w") as file:
-    for val in y_pred:
-        print(val, file=file)
+# with open('final_forest.pkl', 'wb') as file:
+#     pickle.dump(regressor, file)
 
-# evaluate algo performance
+
+# Evaluate algo performance
+y_test[y_test == 0] = .0001
+errors = abs(y_pred - y_test)
+mean = []
+map = 100 * np.mean(errors / y_test)
+accuracy = 100 - map
+
+print('Performance')
+print('Average Error: ${:0.4f}.'.format(np.mean(errors)))
+print('Accuracy = {:0.2f}%.'.format(accuracy))
+print()
 print('Mean Abs Error', metrics.mean_absolute_error(y_test, y_pred))
 print('Mean Sq Error', metrics.mean_squared_error(y_test, y_pred))
 print('Root Mean Sq Error', np.sqrt(metrics.mean_squared_error(y_test, y_pred)))
+
+# Visualize results
+x_test = range(len(y_pred))
+x_train = range(len(y_train_pred))
+x_full = range(len(y_pred) + len(y_train_pred))
+
+fig, axs = plt.subplots(3)
+axs[0].set_title('Bitcoin Price RF Predictions')
+axs[0].scatter(x_train, y_train, c='black')
+axs[0].scatter(x_train, y_train_pred, c='red', marker=".")
+
+axs[1].scatter(x_test, y_test, c='black')
+axs[1].scatter(x_test, y_pred, c='red', marker=".")
+
+axs[2].scatter(x_full, y_orig, c='black')
+axs[2].plot(x_full, y_full, c='red')
+axs[2].set_xlabel('Relative dates from 2012-2016')
+
+plt.show()
